55e9e0e74121c982049b5430b1980284d83ad3bd
Vinay Talla <98005486+VinayTalla20@users.noreply.github.com>
2022-09-19 16:01:18 +0530

    Update file_dir_script.py

--- file_dir_script.py
+++ file_dir_script.py
@@ -1,8 +1,26 @@
 import os
 import sys
+#print(os.getenv("HOSTNAME", default=None))
+#path="/etc/hosts"
+#if os.path.exists(path):
+#    print(f"{path} is valid path")
+#else:
+#    print(f"{path} is Invalid Path")
+#path= input("Enter the Path of Directory to list files :")
+#print(os.path.is_dir())
+#print(os.listdir(path))
+#print(os.getcwd())
+#print(os.listdir())
 
+#print("hello World")
 
 trace_path=input("Enter the path:")
+
+if os.path.exists(trace_path):
+    print(f"{trace_path} is a valid path")
+else:
+    print(f"{trace_path} is not a valid path \n Enter a valid path")
+    sys.exit()
 
 print(f"choosen path {trace_path} ")
 f_d=os.listdir(trace_path)
