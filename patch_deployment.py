ac15c0b2fe0292f0b094643787cbd7b310b2d6ca
Vinay Talla <98005486+VinayTalla20@users.noreply.github.com>
2022-10-31 17:32:48 +0530

    Update patch_deployment.py

--- patch_deployment.py
+++ patch_deployment.py
@@ -7,28 +7,52 @@ deployment_names = []
 f = open("config", 'r')
 file = f.read()
 
+#print("Enter Deployemnt Name:")
+#dep_name = input()
+
+#print(dep_name)
+
+
+
 
 config.load_kube_config(config_file='./config')
 
 dep = client.api.AppsV1Api()
 
-t = open('./patch.json', 'r')
-txt = t.read()
-value = '{"spec": {"replicas": 2}}'
-print(txt)
+#t = open('./patch.json', 'r')
+#txt = t.read()
+#value = '{"spec": {"replicas": 3}}'
+#print(txt)
+
+print("Enter Deployment Name:")
+d_name = input()
+
+print("Enter Namespace:")
+
+ns = input()
+
+print(d_name, ns)
 
 d = dep.list_deployment_for_all_namespaces()
-modify = dep.patch_namespaced_deployment_scale(namespace='default', name='nginx', body=json.loads(value))
+
+#data = dep.list_namespaced_deployment(namespace=f'{ns}')
+val = dep.read_namespaced_deployment(namespace=f'{ns}', name=f'{d_name}')
+
+replica = val.spec.replicas
+
+print(replica)
+
+
+value = '{"spec": {"replicas": '+ f'{replica}' +'}}'
+#txt = '{"spec": {"replicas": 1}}'
+#print(txt)
+print(value)
+
+
+#for n in data.items:
+    #print(n)
+ #   print(n.spec.replicas)
+
+
+modify = dep.patch_namespaced_deployment_scale(namespace=f'{ns}', name=f'{d_name}', body=json.loads(value))
 print(modify)
-
-print('-' *35)
-
-for x in d.items:
-
-    deploy_name = x.metadata.name
-    deployment_names.append(deploy_name)
-
-print(deployment_names)
-
-if 'nginx' in deployment_names:
-    print("success")
